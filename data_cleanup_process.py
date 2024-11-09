3c4dc26f546828d6494b7e84cdaa5331cdfc5b8c
Suffyan Asad <suffyan.asad1@gmail.com>
2024-11-09 23:47:54 +0500

    Create data cleanup step (#1)
    * added gitignore and requirement.txt files
    
    * Fixed pyspark in requirements.txt
    
    * Setup basics, Implemented cleanup of numeric fields, added test code
    
    * Added data cleanup flows and final combining of data
    
    * Added job arguments. Fixed columns mismatch causing union to fail
    
    * Added taxi_type column, removed comments
    
    * Added start and end date parameters

---
+++ data_cleanup_process.py
@@ -0,0 +1,165 @@
+from multiprocessing.managers import Namespace
+from typing import Callable
+
+import argparse
+from pyspark.sql import DataFrame
+import pyspark.sql.functions as F
+
+from utils import create_spark_session, trim_slash
+
+numeric_columns = [
+    "fare_amount", "extra", "mta_tax", "tip_amount", "tolls_amount", "improvement_surcharge", "total_amount",
+    "congestion_surcharge", "airport_fee"
+]
+
+def clean_numeric_fields(data: DataFrame) -> DataFrame:
+    filtered_data = data
+    data_columns = [str(col) for col in data.schema.names]
+    for col_name in numeric_columns:
+        if col_name in data_columns:
+            print(f"Cleaning column {col_name}")
+            filtered_data = filtered_data.filter(F.col(col_name) >= 0.0)
+        else:
+            print(f"Column {col_name} not found in data")
+
+    return filtered_data
+
+
+def clean_out_of_range_data(min_date_str: str, max_date_str: str) -> Callable[[DataFrame], DataFrame]:
+    def clean_out_of_range_data_internal(data: DataFrame) -> DataFrame:
+        print(f"Removing rows with timestamps less than {min_date_str} or greater than {max_date_str}")
+
+        # | is "or" operation in PySpark
+        out_of_range_filter = (
+            (F.col("pickup_datetime") < min_date_str) |
+            (F.col("pickup_datetime") > max_date_str)
+        ) | (
+            (F.col("dropoff_datetime") < min_date_str) |
+            (F.col("dropoff_datetime") > max_date_str)
+        )
+
+        # ~ is "not" operation in PySpark
+        res_data = data.filter(~out_of_range_filter)
+        return res_data
+    return clean_out_of_range_data_internal
+
+
+def process_yellow_trips_data(data_start_time_str, data_end_time_str, data: DataFrame) -> DataFrame:
+    print("Processing yellow trips data")
+
+    res_data = (
+        data
+        .withColumnRenamed("tpep_pickup_datetime", "pickup_datetime")
+        .withColumnRenamed("tpep_dropoff_datetime", "dropoff_datetime")
+        .withColumn("taxi_type", F.lit("yellow"))
+        .withColumn("ehail_fee", F.lit(0.0))
+        .transform(clean_numeric_fields)
+        .transform(clean_out_of_range_data(data_start_time_str, data_end_time_str))
+        .drop("store_and_fwd_flag", "payment_type", "RatecodeID")
+    )
+
+    return res_data
+
+def process_green_trips_data(data_start_time_str, data_end_time_str, data: DataFrame) -> DataFrame:
+    print("Processing green trips data")
+
+    res_data = (
+        data
+        .withColumnRenamed("lpep_pickup_datetime", "pickup_datetime")
+        .withColumnRenamed("lpep_dropoff_datetime", "dropoff_datetime")
+        .withColumn("taxi_type", F.lit("green"))
+        .withColumn("ehail_fee", F.when(F.col("ehail_fee").isNull(), F.lit(0.0)).otherwise(F.col("ehail_fee")))
+        .withColumn("airport_fee", F.lit(0.0))
+        .transform(clean_numeric_fields)
+        .transform(clean_out_of_range_data(data_start_time_str, data_end_time_str))
+        .drop("store_and_fwd_flag", "payment_type", "RatecodeID", "trip_type")
+    )
+
+    return res_data
+
+def parse_job_arguments() -> dict[str, str]:
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        "--yellow_trips_path", required=True, type=str, help="Path of yellow trips data"
+    )
+    arg_parser.add_argument(
+        "--green_trips_path", required=True, type=str, help="Path of green trips data"
+    )
+    arg_parser.add_argument(
+        "--data_start_date", required=True, type=str, help="Start of data date (YYYY-mm-dd)"
+    )
+    arg_parser.add_argument(
+        "--data_end_date", required=True, type=str, help="End of data date (YYYY-mm-dd)"
+    )
+    arg_parser.add_argument(
+        "--output_path", required=True, type=str, help="Path to write output of this step"
+    )
+
+    job_step_args = arg_parser.parse_args()
+    print(job_step_args)
+
+    yellow_trips_path = trim_slash(job_step_args.yellow_trips_path)
+    green_trips_path = trim_slash(job_step_args.green_trips_path)
+    data_start_timestamp_str = f"{job_step_args.data_start_date} 00:00:00"
+    data_end_timestamp_str = f"{job_step_args.data_end_date} 23:59:59"
+    output_path = trim_slash(job_step_args.output_path)
+
+    return {
+        "yellow_trips_path": yellow_trips_path,
+        "green_trips_path": green_trips_path,
+        "data_start_timestamp_str": data_start_timestamp_str,
+        "data_end_timestamp_str": data_end_timestamp_str,
+        "output_path": output_path
+    }
+
+
+if __name__ == '__main__':
+    step_args = parse_job_arguments()
+    print(step_args)
+
+    spark = create_spark_session(app_name="Data Cleanup", is_local=True)
+
+    yellow_data = (
+        spark.read.parquet(step_args["yellow_trips_path"] + "/*.parquet")
+        .withColumnRenamed("tpep_pickup_datetime", "pickup_datetime")
+        .withColumnRenamed("tpep_dropoff_datetime", "dropoff_datetime")
+    )
+
+    green_data = (
+        spark.read.parquet(step_args["green_trips_path"] + "/*.parquet")
+        .withColumnRenamed("lpep_pickup_datetime", "pickup_datetime")
+        .withColumnRenamed("lpep_dropoff_datetime", "dropoff_datetime")
+    )
+
+    print(f"Raw yellow data count {yellow_data.count()}")
+    print(f"Raw green data count {green_data.count()}")
+
+    yellow_data.show(truncate=False, n=10)
+    green_data.show(truncate=False, n=10)
+
+    yellow_data.summary().show(truncate=False)
+    green_data.summary().show(truncate=False)
+
+    clean_yellow_data = process_yellow_trips_data(
+        data_start_time_str=step_args["data_start_timestamp_str"],
+        data_end_time_str=step_args["data_end_timestamp_str"],
+        data=yellow_data
+    )
+
+    clean_green_data = process_green_trips_data(
+        data_start_time_str=step_args["data_start_timestamp_str"],
+        data_end_time_str=step_args["data_end_timestamp_str"],
+        data=green_data
+    )
+
+    clean_yellow_data.summary().show(truncate=False)
+    clean_green_data.summary().show(truncate=False)
+
+    clean_yellow_data.printSchema()
+    clean_green_data.printSchema()
+
+    combined_data = clean_yellow_data.unionByName(clean_green_data)
+
+    print(f"Final data count: {combined_data.count()}")
+
+    combined_data.write.option("overwrite", "true").parquet(step_args["output_path"])
---
+++ utils.py
@@ -0,0 +1,30 @@
+from pyspark import SparkConf
+from pyspark.sql import SparkSession
+
+
+def create_spark_session(app_name: str, is_local: False) -> SparkSession:
+    conf = (
+        SparkConf()
+        .set("spark.driver.memory", "8g")
+        .set("spark.sql.session.timeZone", "UTC")
+    )
+
+    if is_local:
+        spark_session = SparkSession\
+            .builder\
+            .master("local[*]")\
+            .config(conf=conf)\
+            .appName(app_name) \
+            .getOrCreate()
+    else:
+        spark_session = SparkSession \
+            .builder \
+            .config(conf=conf) \
+            .appName(app_name) \
+            .getOrCreate()
+
+    return spark_session
+
+
+def trim_slash(text: str):
+    return text[:-1] if text.endswith("/") else text
